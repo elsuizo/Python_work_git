a4f16e78207e77c2cf15e82e0460446898aa4fad
Martin Noblía <elsuizo@elsuizo-desktop.(none)>
2014-03-18 15:33:18 -0300

    actualizacion

---
+++ person2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+
+class Person:
+    def __init__(self, name, job=None, pay=0):
+
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+    def lastName(self):
+
+        return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+
+        self.pay = int(self.pay * (1 + percent))
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=10000)
+    print bob.name, bob.pay
+    print sue.name, sue.pay
+    print bob.lastName()
+    sue.giveRaise(.10)
+    print sue.pay
